a2c1905425fd32f9dbedeec6747834d1eddffb40
taham8875 <taha.m8875@gmail.com>
2023-07-17 17:07:32 +0300

    input validation

--- src/components/shorten-form.tsx
+++ src/components/shorten-form.tsx
@@ -1,32 +1,52 @@
 import { GetServerSideProps } from "next";
-
-const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  // console log the text user entered
-
-  console.log(e.currentTarget[0].value);
-};
+import { useState } from "react";
+import { api } from "~/utils/api";
 
 const ShortenForm = () => {
+  const createShortUrl = api.shortenedUrl.create.useMutation();
+
+  const [inputValue, setInputValue] = useState("");
+  const [warningMessage, setWarningMessage] = useState("");
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+
+    if (!urlRegex.test(inputValue)) {
+      setWarningMessage("Please enter a valid URL");
+    } else {
+      setWarningMessage("");
+    }
+  };
+
   return (
-    <div className="flex items-center justify-center">
-      <form
-        onSubmit={submitHandler}
-        className="flex items-center justify-center rounded-l-lg "
-      >
-        <input
-          type="text"
-          className="w-96 rounded-l-lg bg-slate-950 px-4 py-2  ring-2 ring-slate-700 focus:outline-none"
-          placeholder="Enter your long URL"
-        />
-        <button
-          type="submit"
-          className="rounded-r-lg bg-blue-500 px-4 py-2 text-white ring-2 ring-slate-700 hover:bg-blue-600"
+    <>
+      <div className="flex items-center justify-center">
+        <form
+          onSubmit={submitHandler}
+          className="flex items-center justify-center rounded-l-lg "
         >
-          Shorten
-        </button>
-      </form>
-    </div>
+          <input
+            type="text"
+            className="w-96 rounded-l-lg bg-slate-950 px-4 py-2  ring-2 ring-slate-700 focus:outline-none"
+            placeholder="Enter your long URL"
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="rounded-r-lg bg-blue-500 px-4 py-2 text-white ring-2 ring-slate-700 hover:bg-blue-600"
+          >
+            Shorten
+          </button>
+        </form>
+      </div>
+      {warningMessage && (
+        <div className="mt-6 flex justify-center text-red-400">
+          {warningMessage}
+        </div>
+      )}
+    </>
   );
 };
 
